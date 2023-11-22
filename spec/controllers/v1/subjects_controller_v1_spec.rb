54b91a1e298996d11e879ea1dd9ee89ef78867aa
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-22 11:31:27 -0300

    12° commit - tdd controller: Consegue retornar um subject especifico e status 200? RED

--- spec/controllers/v1/subjects_controller_v1_spec.rb
+++ spec/controllers/v1/subjects_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::SubjectsController, type: :controller do
     end
   end
 
+  describe "GET /api/v1/subjects/id" do
+    it "Consegue retornar um subject especifico e status 200?" do
+      get :show, params: {id: @subject.id}
+      expect(response.body).to include_json(id: @subject.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
