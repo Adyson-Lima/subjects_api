3d9915ec6361891eb57c64d71cb5be2078d3b270
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-11-22 10:47:59 -0300

    2° commit - tdd model: name consegue ser preenchido? RED

---
+++ spec/models/subject_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Subject, type: :model do
+
+  before{@subject = Subject.new}
+
+  describe "Teste de preenchimento do model Subject" do
+
+    it "name consegue ser preenchido?" do
+      @subject = Subject.name = ""
+      expect(@subject.name).to eq("Matemática")
+    end
+
+  end
+
+end
